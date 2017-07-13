502999a5120d7e6b4fe5579adca51a5c898399b7
eriixon <radkovsn@gmail.com>
2017-07-13 09:56:34 -0600

    added solution

---
+++ screening/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace screening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {0,9,8,3,3,5,9,5,0};
+            numbers = SimplifyNumbers.Simplify(numbers);
+            foreach(var num in numbers)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadLine();
+        }
+
+        public static class SimplifyNumbers
+        {
+            public static int[] Simplify (int[] numbers){
+                Array.Sort(numbers);
+                return numbers.Distinct().ToArray();
+            }
+        }
+    }
+}
